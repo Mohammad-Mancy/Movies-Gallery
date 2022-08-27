7396627dd8ec027bc085a7727fa1648b628702e5
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-08-27 14:03:02 +0300

    Appling fetch API Top rated movies

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -2,29 +2,30 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import notFoundImage from './../assets/notfound.png'
 
-function BasicExample() {
+function TopMovieCard({title,overview,image,release_date,vote_average,original_language}) {
   return (
-    <Card style={{ width: '30rem' }}>
-    <Card.Img variant="top" src={notFoundImage} />
-    <Card.ImgOverlay>
-        <Card.Text>Release date</Card.Text>
-        <Card.Text>Lang</Card.Text>
+    <Card style={{ width: '30rem', alignSelf:'flex-start'}}>
+      {image !== ''?
+      <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${image}`}/>
+      :
+      <Card.Img variant="top" src={notFoundImage} />
+      }
+
+    <Card.ImgOverlay style={{color:'#fff',fontWeight:'Bold'}}>
+        <Card.Text>{release_date}</Card.Text>
+        <Card.Text>{original_language}</Card.Text>
     </Card.ImgOverlay>
-      <Card.Body>
+      <Card.Body style={{height:'30vh'}}>
         <div className='movie-card-title'>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text><strong>8.7</strong></Card.Text>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text><strong>{vote_average}</strong></Card.Text>
         </div>
 
-        <Card.Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque qui accusantium nihil 
-            facilis animi ullam consequuntur architecto numquam necessitatibus natus nisi facere 
-            debitis eius, eos voluptatem vel magnam omnis eaque.
-        </Card.Text>
+        <Card.Text  style={{ height: '46%' , overflow: 'hidden' }}>{overview}</Card.Text>
         <Button variant="primary">View Details</Button>
       </Card.Body>
     </Card>
   );
 }
 
-export default BasicExample;
+export default TopMovieCard;
