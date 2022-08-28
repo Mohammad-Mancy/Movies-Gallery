ac80cbe3782c0fb8017953cd5730aa14e8035467
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-08-28 10:00:05 +0300

    Adding Signup Form

--- frontend/src/components/modal/Signup.jsx
+++ frontend/src/components/modal/Signup.jsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
-import Signup from './Signup';
 
-function Login() {
+function Signup() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -12,37 +11,43 @@ function Login() {
 
   return (
     <>
-      <Button variant="link" onClick={handleShow} className='login-btn'>
-        Login
-      </Button>
+      <h6 onClick={handleShow} className='signup-link'>Create New Account</h6>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Login Form</Modal.Title>
+          <Modal.Title>Signup Form</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Full Name"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" >
               <Form.Label>Email address</Form.Label>
               <Form.Control
                 type="email"
                 placeholder="name@example.com"
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3">
               <Form.Label>Password</Form.Label>
               <Form.Control
                 type="password"
+                placeholder="min 6 character"
               />
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-            <Signup />
+          <h6 className='signup-link' onClick={handleClose}>Already Have An Account</h6>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
-            Login
+            Signup
           </Button>
         </Modal.Footer>
       </Modal>
@@ -50,4 +55,4 @@ function Login() {
   );
 }
 
-export default Login
+export default Signup
