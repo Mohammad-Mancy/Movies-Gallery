00fcb573fa30f0dcfe293a087caa1a1863ed481e
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-08-30 17:06:03 +0300

    Updating movie details page fetch and display data

--- frontend/src/components/pages/MovieDetailsPage.jsx
+++ frontend/src/components/pages/MovieDetailsPage.jsx
@@ -1,17 +1,42 @@
 import React, { useEffect } from 'react'
 import NavBar from './../NavBar'
-import image from './../../assets/top-rated.jpg'
 import Button from 'react-bootstrap/Button';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import Badge from 'react-bootstrap/Badge';
+import Spinner from 'react-bootstrap/Spinner';
 
 
 function MovieDetailsPage() {
     const location = useLocation()
-    console.log(location.state.id)
+    
+    const [poster_path,setPoster_path] = useState()
+    const [release_date,setRelease_date] = useState()
+    const [overview,setOverview] = useState()
+    const [original_language,setOriginal_language] = useState()
+    const [title,setTitle] = useState()
+    const [vote_average,setVote_average] = useState()
+    const [backdrop_path,setBackdrop_path] = useState()
 
-    let handleMoviesDetails = () => {
+    let handleMoviesDetails = async () => {
         try {
-            // Logic goes here      
+            let res = await fetch('http://localhost:3000/api/movie/details',{
+                method: 'POST' ,
+                headers : {'Content-Type' : 'application/json'},
+                body : JSON.stringify({
+                    id: location.state.id
+                })
+            })   
+            const data = await res.json()
+            if (res.status === 200) {
+                setPoster_path(data.poster_path)
+                setRelease_date(data.release_date)
+                setOverview(data.overview)
+                setOriginal_language(data.original_language)
+                setTitle(data.title)
+                setVote_average(data.vote_average)
+                setBackdrop_path(data.backdrop_path)
+            }
         } catch (error) {
             console.log(error)
         }
@@ -19,21 +44,27 @@ function MovieDetailsPage() {
     // fetch the data of movie on page load
     useEffect ( () => {
         handleMoviesDetails();
-      },[]);
+      });
+    if(!poster_path||!vote_average||!backdrop_path) {
+        return <Spinner animation="grow" />;
+    }
   return (
     <div>
         <NavBar />
-        <div className='background-image'>
+        <div className='background-image' style={{backgroundImage:`url("https://image.tmdb.org/t/p/w500/${backdrop_path}")`}}>
         <div className='background-image-inner'>
             <div className='inner-image'>
-                <img src={image} className="poster-image"/>
+                <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} className="poster-image"/>
                 <div className='movie-details'>
-                    <h1>Movie Title</h1>
-                    <h3><span><strong>2022/12/12</strong><span> ( US )</span></span></h3>
+                    <div style={{display:'flex'}}>
+                        <h1>{title}</h1>
+                        <Badge pill bg="warning" text="dark" style={{height:'min-content'}}>
+                            <strong>{vote_average.toFixed(1)}</strong>
+                        </Badge>    
+                    </div>
+                    <h3><span><strong>{release_date}</strong><span> ( {original_language} )</span></span></h3>
                     <h4 style={{color:'slategrey'}}>Overview</h4>
-                    <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi,
-                         possimus nihil. Dicta inventore deserunt ut voluptate rem! Culpa officiis quod velit natus
-                          rerum aliquam! Qui ad quod error suscipit numquam?</h4>
+                    <h4>{overview}</h4>
                 </div>
                 <Button variant="outline-secondary" className='add-to-gallery-btn'>
                         Add To My Gallery
