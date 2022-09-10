98e047adf28f36631bd8c22b3226c84b67dd9d18
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-09-10 13:23:30 +0300

    Updating Genres page

--- frontend/src/components/pages/GenresPage.jsx
+++ frontend/src/components/pages/GenresPage.jsx
@@ -3,11 +3,34 @@ import { Footer } from "./../Footer";
 import NavBar from "./../NavBar"
 
 function GenresPage() {
+
+  const [genres,setGenres] = React.useState([])
+
+  let handleMoviesGenres = async () => {
+    try {
+      let res = await fetch('http://localhost:3000/api/movies/genres_list',{
+        method: 'GET' ,
+        headers : {'Content-Type' : 'application/json'}
+    })   
+    const data = await res.json()
+    if (res.status === 200) {
+      setGenres(data.response)
+    }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  React.useEffect ( () => {
+    handleMoviesGenres();
+  },[]);
+
   return (
     <div>
         <NavBar />
         <div className="genres-wrapper">
-            Genres
+            {genres.map(({id,name})=>(
+              <span className='genre-card' key={id}>{name}</span>
+            ))}
         </div>
         <Footer />
     </div>
